11037c61ec284d01dcf5628c26cf5b6d1191fc8a
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-11 14:24:27 +0500

    Add files via upload

---
+++ Day8.cpp
@@ -0,0 +1,116 @@
+//Program # 1: DMAS rule.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     float b = 6.6/7+2*3;
+//     cout<<"b is equal to: "<<b<<".";
+// }
+
+//Program # 2: DMAS rule.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     float d = 7/22*(3.14+2)*3/5;
+//     cout<<"d is equal to: "<<d<<".";
+// }
+
+//Program # 4: DMAS rule
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     float f = 4+2%-8;
+//     cout<<"f is equal to: "<<f<<".";
+// }
+
+//Program # 5: Even and odd number.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<"Please! Enter a number: ";
+//     cin>>n;
+//     if((n % 2) == 0) {
+//         cout<<"The number "<<n<<" is Even.";
+//     }
+//     else if((n % 2) != 0) {
+//         cout<<"The number "<<n<<" is Odd.";
+//     };
+// }
+
+//Program # 6: 
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int costPrice;
+//     int salePrice;
+//     cout<<"Please! Enter Cost Price: ";
+//     cin>>costPrice;
+//     cout<<"Please! Enter Sale Price: ";
+//     cin>>salePrice;
+//     if(costPrice<salePrice) {
+//         cout<<"Profit!";
+//     }
+//     else if(costPrice>salePrice) {
+//         cout<<"Loss!";
+//     }
+//     else if(costPrice == salePrice) {
+//         cout<<"No Profit No Loss!";
+//     }
+// }
+
+//Program # 7: Area of a rectangle is greater or less than perimeter.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int areaOfrectangle;
+//     int perimeterOfrectangle;
+//     cout<<"Please! Enter area of rectangle: ";
+//     cin>>areaOfrectangle;
+//     cout<<"Please! Enter Perimeter of rectangle: ";
+//     cin>>perimeterOfrectangle;
+//     if(areaOfrectangle>perimeterOfrectangle) {
+//         cout<<"Area of rectange is greater than it's perimeter.";
+//     }
+//      if(areaOfrectangle<perimeterOfrectangle) {
+//         cout<<"Area of rectange is less than it's perimeter.";
+//     }
+// }
+
+//Program # 8: Modified program for Area of a rectangle is greater or less than perimeter.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int length;
+//     int breadth;
+//     int areaOfrectangle;
+//     int perimeterOfrectangle;
+//     cout<<"Please! Enter length of rectange: ";
+//     cin>>length;
+//     cout<<"Please! Enter breadth of rectangle: ";
+//     cin>>breadth;
+//     areaOfrectangle = length*breadth;
+//     perimeterOfrectangle = 2*(length*breadth);
+//     if(areaOfrectangle>perimeterOfrectangle) {
+//         cout<<"Area of rectangle = "<<areaOfrectangle<<endl;
+//         cout<<"Perimeter of rectangle = "<<perimeterOfrectangle<<endl;
+//         cout<<"So, The Area of rectangle is greater than it's perimeter.";
+//     }
+//      if(areaOfrectangle<perimeterOfrectangle) {
+//         cout<<"Area of rectangle = "<<areaOfrectangle<<endl;
+//         cout<<"Perimeter of rectangle = "<<perimeterOfrectangle<<endl;
+//         cout<<"So, The Area of rectange is less than it's perimeter.";
+//     }
+// }
+
+
+
+
+
+
